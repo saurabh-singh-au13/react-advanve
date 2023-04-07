4aa994da22d282b952c097babe2d486981f83cd1
Saurabh Kumar Singh <s.rajvansh.1996@gmail.com>
2023-04-07 14:10:29 +0530

    children props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 function App() {
   return (
    <>
-   <Props name="Saurabh" resources="code007" />
+   {/* <Props name="Saurabh" resources="code007" />
    <Props name="in React.js" resources="by Saurabh" />
-   <Props name="JavaScript" resources="by codeWithSaurabh" />
+   <Props name="JavaScript" resources="by codeWithSaurabh" /> */}
+   <Props name="React">
+      <h1> Hello Saurabh </h1>
+   </Props>
 
    </>
   );
--- src/components/Props.js
+++ src/components/Props.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./Props.css"
 
-const Props = ({name, resources}) => {
+
+const Props = (props) => {
 
   return (
     <>
-    <h1>This is Props {name} and {resources} </h1>
+    {/* <h1>This is Props {name} and {resources} </h1> */}
+    <p>Here we will pass a props to childeren <span> {props.name} </span> </p>
+    <h3>
+    {
+      props.children
+    }
+    </h3>
     </>
   )
 }
