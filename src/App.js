571c1730a9ff18312e35090b0bed3aaf959a0556
Saurabh Kumar Singh <s.rajvansh.1996@gmail.com>
2023-04-07 13:57:25 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import Props from './components/Props';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Props name="Saurabh" resources="code007" />
+   <Props name="in React.js" resources="by Saurabh" />
+   <Props name="JavaScript" resources="by codeWithSaurabh" />
+
+   </>
   );
 }
 
---
+++ src/components/Props.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Props = ({name, resources}) => {
+
+  return (
+    <>
+    <h1>This is Props {name} and {resources} </h1>
+    </>
+  )
+}
+
+export default Props
